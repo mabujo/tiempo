4b7df6d7c80ec05b29af7f1304f60d168c8df2c9
mabujo <john@mabujo.com>
2014-11-06 20:41:10 +0100

    add wunderground weather lib

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,23 @@
 <?php
 
 require '../vendor/autoload.php';
+require '../wunderground/Wunderground.php';
+
+$weather = new Wunderground("bdff1e0a39d8035f", $lang);
+
+// cache dir
+$weather->setCacheDir(MY_PATH . 'tmp/');
+$weather->setCacheExpiry(3600);
+
+$city = "Malaga";
+$country = "Spain";
+
+// get current weather
+var_dump($weather->getCurrentWeather($city, $country));
+
+// get forecast
+$weather->getForecast($city, $country);
+
 
 ?>
 
