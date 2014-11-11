13dbd81ac0a3528b5cc45ce864bd5726160a4cbe
mabujo <john@mabujo.com>
2014-11-11 20:53:36 +0100

    daytime weather mappings

--- functions.php
+++ functions.php
@@ -1,5 +1,10 @@
 <?php
-
+	
+	/*
+	 * See if it is day or night in the location while ignoring local server time.
+	 * 
+	 * Returns boolean true if day (between 6am and 8pm), false if night.
+	 */
 	function spainIsDay() 
 	{
 	    date_default_timezone_set('Europe/Madrid');
@@ -8,15 +13,86 @@
 	
 	    if ($time >= 6 && $time <= 20) 
 	    {
+			echo "day";
 			return true;
 	    }
 	    else {
+			echo "night";
 			return false;
 	    }
 	}
 	
-	function mapWeatherToIcon() 
+	/*
+	 * Take the weather condition from the weather provider and map it to 
+	 * an icon in the Weather Icons fontset.
+	 * Takes day or night into account, setting the correct icon.
+	 * Is basically a horrorshow.
+	 * 
+	 * Returns the icon CSS class.
+	 */
+	function mapWeatherToIcon($weatherCondition = "Clear") 
 	{
+		// Daytime
+		if(spainIsDay())
+		{
+			// Clear / unknown
+			if($weatherCondition == "Clear" || $weatherCondition == "Unkown" )
+			{
+				$weatherIcon = "wi-day-sunny";
+			}
+			// Sort of cloudy
+			elseif($weatherCondition == "Partly Cloudy" || $weatherCondition == "Scattered Clouds")
+			{
+				$weatherIcon = "wi-day-cloudy";
+			}
+			// Very cloudy
+			elseif($weatherCondition == "Mostly Cloudy" || $weatherCondition == "Overcast")
+			{
+				$weatherIcon = "wi-cloudy";
+			}
+			// Lots of rain
+			elseif($weatherCondition == "Heavy Rain" || stripos($weatherCondition, 'Rain Mist') !== false)
+			{
+				$weatherIcon = "wi-rain";
+			}
+			// Some rain
+			elseif($weatherCondition == "Light Rain" || $weatherCondition == "Unknown Precipitation" || stripos($weatherCondition, 'drizzle') !== false || stripos($weatherCondition, 'Rain') )
+			{
+				$weatherIcon = "wi-rain-mix"; 
+			}
+			// Storms
+			elseif(stripos($weatherCondition, 'Thunderstorm') !== false)
+			{
+				$weatherIcon = "wi-thunderstorm";
+			}
+			// Snow or ice
+			elseif(stripos($weatherCondition, 'Snow') !== false || stripos($weatherCondition, 'Ice') !== false)
+			{
+				$weatherIcon = "wi-day-snow";
+			}
+			// Hail
+			elseif(stripos($weatherCondition, 'Hail') !== false)
+			{
+				$weatherIcon = "wi-hail";
+			}
+			// Fog
+			elseif(stripos($weatherCondition, 'Fog') !== false || stripos($weatherCondition, 'Mist') !== false)
+			{
+				$weatherIcon = "wi-day-fog";
+			}
+			// Nondescript
+			else 
+			{
+				$weatherIcon = "wi-day-cloudy";
+			}
+		}
+		// Nightime is better than daytime
+		else
+		{
+			
+		}
+		
+		return $weatherIcon;
 
 	}
 
--- public_html/index.php
+++ public_html/index.php
@@ -1,45 +1,8 @@
 <?php
-
+//init
 require '../vendor/autoload.php';
 require '../wunderground/Wunderground.php';
 require '../functions.php';
-
-$weather = new Wunderground("bdff1e0a39d8035f", "en");
-
-// cache dir
-$weather->setCacheDir(getcwd() . '/cache/');
-$weather->setCacheExpiry(1000);
-
-$city = "Estepona";
-$country = "Spain";
-
-// get current weather
-$weatherCurrent = $weather->getCurrentWeather($city, $country);
-
-// get forecast
-$weatherForecast = $weather->getForecast($city, $country);
-
-//var_dump($weatherCurrent);
-//var_dump($weatherForecast);
-
-//temperature
-$temp = $weatherCurrent->current_observation->temp_c;
-
-//weather condition, clear, cloudy e.t.c.
-$weatherCondition = $weatherCurrent->current_observation->weather;
-
-	switch ($weatherCondition) 
-	{
-	    case "clear":
-	        $weatherIcon = "wi-day-sunny";
-	        break;
-	    case "Partly Cloudy":
-	        $weatherIcon = "wi-day-sunny-overcast";
-	        break;
-		default:
-			$weatherIcon = "wi-day-cloudy";
-	}
-
 
 //slim init
 $tiempo = new \Slim\Slim(array(
@@ -47,6 +10,31 @@ $tiempo = new \Slim\Slim(array(
 	'log.enabled' => true
 ));
 
+//weather provider object
+$weather = new Wunderground("bdff1e0a39d8035f", "en");
+
+//cache dir
+$weather->setCacheDir(getcwd() . '/cache/');
+$weather->setCacheExpiry(1000);
+
+//location
+$city = "Estepona";
+$country = "Spain";
+
+//get current weather
+$weatherCurrent = $weather->getCurrentWeather($city, $country);
+
+//get forecast
+$weatherForecast = $weather->getForecast($city, $country);
+
+//temperature
+$temp = $weatherCurrent->current_observation->temp_c;
+
+//weather condition, clear, cloudy e.t.c.
+$weatherCondition = $weatherCurrent->current_observation->weather;
+
+//setup 
+$weatherIcon = mapWeatherToIcon($weatherCondition);
 ?>
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
@@ -57,7 +45,7 @@ $tiempo = new \Slim\Slim(array(
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<title>Marbella Weather</title>
-        <meta name="description" content="">
+        <meta name="description" content="Marbella current weather conditions and 3 day forecast">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
 		<link rel="stylesheet" href="/assets/css/style.css">
@@ -65,10 +53,13 @@ $tiempo = new \Slim\Slim(array(
     <body>
 
 	<div id="fullpage">
-		<div id="firstSlide" class="section">
+		<div id="firstSlide" class="section <?php if(spainIsDay()) { echo "bgDay"; } else { echo "bgNight"; } ?>">
 			<div class="container">
 				<div id="weatherIcon">
 					<i class="wi <?php echo $weatherIcon; ?>"></i>
+				</div>
+				<div>
+					<?php echo $weatherCondition; ?>
 				</div>
 			</div>
 		</div>
