4f406fead0e939001b84cb03c61b1e394b9c0d3c
John Henson <john@mabujo.com>
2014-11-12 16:01:15 +0100

    some tidying up

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,27 +1,27 @@
 	//init scroll vars
 	var scrollUpAmount = 0;
 	var scrollDownAmount = 0;
-	
+
 	//make first slide active
 	$( ".section:first-of-type" ).addClass( "active" );
 
-	$("html, body").bind({'mousewheel DOMMouseScroll onmousewheel touchmove scroll': 
+	$("html, body").bind({'mousewheel DOMMouseScroll onmousewheel touchmove scroll':
 	    function(e) {
 	        if (e.target.id == 'el') return;
 	        e.preventDefault();
 	        e.stopPropagation();
-	
+
 	        //Determine Direction
 	        if (e.originalEvent.wheelDelta && e.originalEvent.wheelDelta >= 0) {
 				scrollUpAmount++;
-	
+
 	        } else if (e.originalEvent.detail && e.originalEvent.detail <= 0) {
 	           scrollUpAmount++;
-	
+
 	        } else {
 	            scrollDownAmount++;
 	        }
-	        
+
 	        if(scrollUpAmount > 5)
 	        {
 				$('.active').removeClass(function(){
@@ -29,7 +29,7 @@
 					{
 						scrollUpAmount = 0;
 						scrollDownAmount = 0;
-						$(this).animate({ "margin-left": "+=100%" }, { duration: "slow", easing: "easeOutBack" } );
+						$(this).animate({ "margin-left": "+=100%" }, { duration: 800, easing: "easeOutBack" } );
 						$(this).prev().addClass('active');
 						return 'active';
 					}
@@ -42,13 +42,13 @@
 						{
 							scrollDownAmount = 0;
 							scrollUpAmount = 0;
-							$(this).next().animate({ "margin-left": "-=100%" }, { duration: "slow", easing: "easeInBack" } );
+							$(this).next().animate({ "margin-left": "-=100%" }, { duration: 800, easing: "easeInBack" } );
 							$(this).next().addClass('active');
 							return 'active';
 						}
 					})
-				
+
 			}
 	    }
 	});
-	
+
